e4bfd1e9410d1269e92a47522d5c75ff29706d7b
jimboweb <jimboweb@users.noreply.github.com>
2018-03-16 18:56:07 -0400

    too slow graph maker

--- src/main/java/KUniversalString.java
+++ src/main/java/KUniversalString.java
@@ -1,22 +1,41 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class KUniversalString {
-    private List<String> strings = new ArrayList<>();
-
-    public static void main(String[] args){
-        KUniversalString kUniversalString = new KUniversalString();
-        kUniversalString.run();
-
-    }
 
     private void run(){
         int k = getInput();
-        strings = createBinaryStrings(k);
+        List<String> binaryStrings = createBinaryStrings(k);
+        List<DeBruijnNode> nodes = makeDeBruijnGraph(binaryStrings, k);
+    }
+
+    private List<DeBruijnNode> makeDeBruijnGraph(List<String> binaryStrings, int k){
+        List<DeBruijnNode> nodes = new ArrayList<>();
+        List<String> kMerOverlaps = createBinaryStrings(k-1);
+        for(int i=0;i<kMerOverlaps.size();i++){
+            nodes.add(new DeBruijnNode(i,kMerOverlaps.get(i)));
+        }
+
+        //this is too slow because 2^14^2 = like 270 billion
+        //I'm gonna have to search with the edges
+        for(int i=0;i<binaryStrings.size();i++){
+            for(int j=0;j<binaryStrings.size();j++){
+                String prefixString = binaryStrings.get(i);
+                String suffixString = binaryStrings.get(j);
+                String prefixStringOl = prefixString.substring(1);
+                String sufficStringOL = suffixString.substring(0,suffixString.length()-2);
+                if(prefixStringOl.equals(sufficStringOL)){
+                    String suffixStringSuffix = suffixString.substring(1);
+                    int firstNodeIndex = Integer.parseInt(sufficStringOL, 2);
+                    int secondNodeIndes = Integer.parseInt(suffixStringSuffix,2);
+
+                }
+            }
+        }
+        return nodes;
     }
 
     private List<String> createBinaryStrings(int k) {
+        List<String> binaryStrings = new ArrayList<>();
         int numStrings = 1<<k;
         for(int i=0;i<numStrings;i++){
             String nextString = "";
@@ -25,13 +44,41 @@ public class KUniversalString {
                 nextString+=nextChar;
             }
             //System.out.println(nextString);
-            strings.add(nextString);
+            binaryStrings.add(nextString);
         }
-        return strings;
+        return binaryStrings;
     }
+
+    private class DeBruijnNode {
+        int index;
+        String str;
+        Deque<Integer> connectedNodes;
+
+        public DeBruijnNode(int index, String str) {
+            this.index = index;
+            this.str = str;
+            connectedNodes = new ArrayDeque<>();
+        }
+
+        public void pushConnectedNode(Integer node){
+            connectedNodes.push(node);
+        }
+
+        public Integer popConnectedNode(){
+            return connectedNodes.pollLast();
+        }
+    }
+    public static void main(String[] args){
+        KUniversalString kUniversalString = new KUniversalString();
+        kUniversalString.run();
+
+    }
+
 
     private int getInput(){
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
+
+
 }
